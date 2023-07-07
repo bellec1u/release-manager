4a3fcb9980a04b60a6ff445bf9c50029dfa3f6b6
Leopold Bellec <leopold.bellec@gmail.com>
2023-07-07 07:02:47 +0200

    implement function

--- functions/release-extractor/index.js
+++ functions/release-extractor/index.js
@@ -1,4 +1,10 @@
-const axios = require('axios').default;
+const axios = require('axios');
+
+const gitlabApiUrl = process.env.GITLAB_API_URL
+const jiraApiUrl = process.env.JIRA_API_URL
+const issueRegex = new RegExp(process.env.ISSUE_REGEX, 'g')
+const requestMaxResults = 10000
+const defaultNumberOfTags = 10
 
 /**
  * Your HTTP handling function, invoked with each request. This is an example
@@ -16,11 +22,30 @@ const axios = require('axios').default;
  * @param {string} context.method the HTTP request method
  * @param {string} context.httpVersion the HTTP protocol version
  * See: https://github.com/knative/func/blob/main/docs/function-developers/nodejs.md#the-context-object
+ *
+ * Function flows:
+ * 1. if fromTag & toTag query parameters are mentioned, the function will return the delta tickets between the two releases
+ * 2. else, the function will return the numberOfTags latest releases
+ *
  */
 const handle = async (context) => {
 
+  if (context.method !== 'GET') {
+    return { statusCode: 405, message: 'Method not allowed' }
+  }
+
   // credential extraction
-  let authorization = JSON.parse(context.headers['authorization'].split(' ')[1])
+  /*
+   * Authorization header format in base 64:
+   * {
+   *   gitlab: <gitlab token>,
+   *   jira: {
+   *     username: <jira username>,
+   *     password: <jira password>
+   *   }
+   * }
+   */
+  let authorization = JSON.parse(Buffer.from(context.headers['authorization'], 'base64').toString())
   let gitlabToken = authorization['gitlab']
   let jiraCredentials = authorization['jira']
 
@@ -28,84 +53,113 @@ const handle = async (context) => {
   // test connection to jira
 
   // query parameter extraction
-  let projectId = context.query['project']
+  let projectId = context.query['projectId']
   if (!projectId) {
-    return { statusCode: 400, statusMessage: 'The "project" parameter is missing' }
+    return { statusCode: 400, message: 'The "project" parameter is missing' }
   }
 
-  let numberOfTags = Number(context.query['tags'])
-  numberOfTags = numberOfTags ? numberOfTags : 10
+  let fromTagNumber = context.query['fromTag']
+  let toTagNumber = context.query['toTag']
+
+  let numberOfTags = Number(context.query['numberOfTags'])
+  numberOfTags = numberOfTags ? numberOfTags : defaultNumberOfTags
+  // we need to increase the number of tags by one in order to retrieve all requested tags
+  numberOfTags++
 
   // extract gitlab last tags
-  // latest release + numberOfTags-1 last releases
-  let tags = await getProjectTags(gitlabToken, projectId)
-  let commits = tags.length < numberOfTags
-      ? await getProjectCommits(gitlabToken, projectId)
-      : await getProjectCommits(gitlabToken, projectId, tags[numberOfTags - 2]['commit']['committed_date'])
+  let tags
+  try {
+    if (fromTagNumber && toTagNumber) {
+      tags = [
+        (await getProjectTag(gitlabToken, projectId, toTagNumber)).data,
+        (await getProjectTag(gitlabToken, projectId, fromTagNumber)).data
+      ]
+    } else {
+      tags = (await getProjectTags(gitlabToken, projectId, numberOfTags)).data
+    }
+  } catch (e) {
+    return { statusCode: 500, message: 'An error occurred while retrieving the project tags' }
+  }
+
+  let commits
+  try {
+    if (fromTagNumber && toTagNumber) {
+      commits = (await getProjectCommits(gitlabToken, projectId, tags[1]['commit']['committed_date'], tags[0]['commit']['committed_date'])).data
+    } else {
+      commits = (tags.length < numberOfTags
+          ? (await getProjectCommits(gitlabToken, projectId))
+          : (await getProjectCommits(gitlabToken, projectId, tags[numberOfTags - 1]['commit']['committed_date'])))
+          .data
+    }
+  } catch (e) {
+    return { statusCode: 500, message: 'An error occurred while retrieving the project commits' }
+  }
 
   // mapping tag with commits
-  let mappedTagCommits = {}
-  let index = -1;
-  let jiraTickets = []
+  let tagsTickets = {}
+  let jiraIssues = []
+
+  let tagIndex = fromTagNumber && toTagNumber ? 0 : -1;
   for (let commit of commits) {
 
-    if (commit['committed_date'] <= tags[index]['commit']['committed_date']) {
-      index++
+    if (tagIndex + 1 < tags.length && commit['committed_date'] < tags[tagIndex + 1]['commit']['committed_date']) {
+      tagIndex++
     }
 
-    let currentTag = index === -1 ? 'Next release' : tags[index]
-    if (!currentTag in mappedTagCommits) {
-      mappedTagCommits[currentTag] = []
+    let currentTag = tagIndex === -1 ? 'Next release' : tags[tagIndex]['name']
+    if (!(currentTag in tagsTickets)) {
+      tagsTickets[currentTag] = []
     }
 
-    let tickets = commits.match(process.env.TICKETS_REGEX)
-    jiraTickets = jiraTickets.concat(tickets)
-    mappedTagCommits[currentTag] = mappedTagCommits[currentTag].concat(tickets)
+    let tickets = commit['title'].match(issueRegex)
+    jiraIssues = tickets ? Array.from(new Set([ ...jiraIssues, ...tickets ])) : jiraIssues
+    tagsTickets[currentTag] = tickets ? Array.from(new Set([ ...tagsTickets[currentTag], ...tickets ])) : tagsTickets[currentTag]
 
   }
 
   // get jira ticket statuses
-  let jiraTicketStatuses = getTicketStatuses(jiraCredentials, jiraTickets)
-
-  // If the request is an HTTP POST, the context will contain the request body
-  if (context.method === 'POST') {
-    return { body }
-  } else if (context.method === 'GET') {
-    // If the request is an HTTP GET, the context will include a query string, if it exists
-    return {
-      query: context.query,
-    }
-  } else {
-    return { statusCode: 405, statusMessage: 'Method not allowed' }
+  let issues
+  try {
+    issues = (await getTicketData(jiraCredentials, jiraIssues)).data['issues']
+  } catch (e) {
+    return { statusCode: 500, message: 'An error occurred while retrieving the jira issues' }
   }
-}
+  let issuesMap = {}
+  issues.forEach(issue => issuesMap[issue['key']] = issue['fields']['status']['name'])
 
-function getTicketStatuses(jiraCredentials, jiraTickets) {
-  let url = process.env.JIRA_URL + ``
-  return axios.get(url, {
-    headers: {
-      'Authorization': `` // todo
+  // map jira issue with tag commits
+  for (let tagTickets in tagsTickets) {
+    for (let ticket in tagsTickets[tagTickets]) {
+      let issue = tagsTickets[tagTickets][ticket]
+      tagsTickets[tagTickets][ticket] = { key: issue, status: issuesMap[issue] }
     }
-  })
+  }
+
+  return tagsTickets
 }
 
-function getProjectCommits(gitlabToken, projectId, since) {
-    let url = process.env.GITLAB_URL + `/projects/${projectId}/repository/commits?all=true`
-      + since ? `&since=${since}` : ``
-    return axios.get(url, {
-      headers: {
-        'Authorization': `Bearer ${gitlabToken}`
-      }
-    })
+function getTicketData(jiraCredentials, jiraTickets) {
+  return axios.get(
+      `${jiraApiUrl}/search?jql=key in (${jiraTickets})&maxResults=${requestMaxResults}&fields=status,subtasks`,
+      { auth: { username: jiraCredentials['username'], password: jiraCredentials['password'] } })
 }
 
-function getProjectTags(gitlabToken, projectId) {
-  let url = process.env.GITLAB_URL + `/projects/${projectId}/repository/tags`;
-  return axios.get(url, {
-    headers: {
-      'Authorization': `Bearer ${gitlabToken}`
-    }
-  })
+function getProjectCommits(gitlabToken, projectId, since, until) {
+  return axios.get(
+      `${gitlabApiUrl}/projects/${projectId}/repository/commits?per_page=${requestMaxResults}` + (since ? `&since=${since}` : ``) + (until ? `&until=${until}` : ``),
+      { headers: { Authorization: `Bearer ${gitlabToken}` } })
+}
+
+function getProjectTags(gitlabToken, projectId, numberOfTags) {
+  return axios.get(
+      `${gitlabApiUrl}/projects/${projectId}/repository/tags?per_page=${numberOfTags}`,
+      { headers: { Authorization: `Bearer ${gitlabToken}` } })
+}
+
+function getProjectTag(gitlabToken, projectId, tagNumber) {
+  return axios.get(
+      `${gitlabApiUrl}/projects/${projectId}/repository/tags/${tagNumber}`,
+      { headers: { Authorization: `Bearer ${gitlabToken}` } })
 }
 
 // Export the function
